217b93fd36b3168cbfee4c095e45c4e17b271b56
kallori <31666322+kallori@users.noreply.github.com>
2021-04-06 18:16:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,23 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
-        
+        // logic to get the actual year
+
+var dateobj = new Date();
+
+var B = dateobj.getFullYear();
+
+year = B+y;
+
+x=principal
+y=rate
+z=years
+interest=principal * years * rate / 100
+
+var y = document.getElementById("years");
+
+var today = new Date();
+
+    var currentYear = today.getFullYear();
+
+    futureYear = currentYear + y;
