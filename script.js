946f602a00349f72b91bdc8ac7f25e45fb48ee02
kallori <31666322+kallori@users.noreply.github.com>
2021-04-06 18:31:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
-function compute() 
+function compute()
 {
+    principal = document.getElementById("principal").value;
     
-   var principal = document.getElementById("principal").value;
-   var rate = document.getElementById("rate").value;
-   var years = document.getElementById("years").value;
+    // checking principal amount for validation
+    if (principal <= 0)
+    {
+        alert("Please input a valid amount for the principal.");
+        document.getElementById("principal").focus;
+        return false;
+    }
+    
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    result = principal * rate * years / 100;
+    // convert number of years into actual year in the future
+    year = parseInt(new Date().getFullYear()) + parseInt(years);
 
-   var interest = principal * years * rate / 100;
-   var current_year = new Date().getFullYear();
-   var future = current_year + parseInt(years);
+    document.getElementById("result").innerHTML = "If you deposit: <mark>" + principal + "</mark><br>At an interest rate of: <mark>" + rate + "%</mark><br>You will receive an amount of: <mark>" + result + "</mark><br>In the year: <mark>" + year + "</mark>";
+    
+ }
 
-   document.getElementById('result').innerHTML = 
-   `If you deposit <span class="highlight">${principal}</span>, <br> 
-   at an interest rate of <span class="highlight">${rate}%</span>. <br>
-   You will receive an amount of <span class="highlight">${interest}</span>, <br>
-   in the year <span class="highlight">${future}</span>`
+ // Using this function to show the slider bar value
+function showVal(newVal){
+    document.getElementById("rate").innerHTML=newVal;
 }
-
-function sliderUpdate()
-{
-    var rate =  document.getElementById("rate").value;
-    document.getElementById('slider_value').innerHTML = `${rate}%`;
-}
-
-function validate()
-{
-const principal_input = document.getElementById("principal");
-var principal = principal_input.value;
-if (Number(principal) <= 0 || principal == ""){
-    alert("Enter a positive number");
-    principal_input.focus();
-    return false;
-}
-
-compute();
-return false;
-}
+        
